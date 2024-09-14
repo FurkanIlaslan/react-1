f97cc5dd7d8f022163b49feb31487f09a08a086d
Furkan <furkanilaslan@gmail.com>
2024-09-14 15:55:42 +0300

    productData.js dosyasının olduğu son durum

--- src/components/NewProduct/ProductForm.jsx
+++ src/components/NewProduct/ProductForm.jsx
@@ -18,22 +18,24 @@ function ProductForm({newProductData, setNewProductData}) {
 
         const newProducts = [...newProductData, newProduct]
         setNewProductData(newProducts)
-
+        setProductTitle("")
+        setProductPrice("")
+        setImageUrl("")
     }
 
   return (
     <form onSubmit={submitHandler} className='product-form'>
         <div className='product-form-input'>
             <label>Ürün Adı</label>
-            <input onChange={(e)=> setProductTitle(e.target.value)} type="text" placeholder='Ürün adı giriniz..'/>
+            <input onChange={(e)=> setProductTitle(e.target.value)} type="text" placeholder='Ürün adı giriniz..' value={productTitle}/>
         </div>
         <div className='product-form-input'>
             <label>Ürün Fiyatı</label>
-            <input onChange={(e)=> setProductPrice(e.target.value)} type="number" placeholder='Ürün fiyatı giriniz..'/>
+            <input onChange={(e)=> setProductPrice(Number(e.target.value))} type="number" placeholder='Ürün fiyatı giriniz..' value={productPrice}/>
         </div>
         <div className='product-form-input'>
             <label>Ürün Görseli</label>
-            <input onChange={(e)=> setImageUrl(e.target.value)} type="text" placeholder='Ürün görseli giriniz..'/>
+            <input onChange={(e)=> setImageUrl(e.target.value)} type="text" placeholder='Ürün görseli giriniz..' value={imageUrl}/>
         </div>
         <button className='product-form-button'>Ürün Ekle</button>
     </form>
